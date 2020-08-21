5bf6265fa6b658536faf6a33d1aefe8b237f7c78
bibi6666667 <bibi6666667@github.com>
2020-08-21 15:24:32 +0900

    1/7/30 레이아웃 열고닫기
    <a><img></a>에 onclick함수 연결하고 함수로 구현.

--- my_project/app.py
+++ my_project/app.py
@@ -48,7 +48,7 @@ def make_sche():
 
 # 2. 일정 조회(Read) - /readsche (GET)
 @app.route('/readsche', methods=['GET'])
-def make_sche():
+def read_sche():
     todolist = db.todo.find({},{'_id' : 0});
     return jsonify({'result': 'success', 'todolist': todolist,'msg': '일정을 불러왔습니다. 멍!'})
 
