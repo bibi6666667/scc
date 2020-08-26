ba86a4a596bb75290d2c49f33e02911e1ee37506
bibi6666667 <bibi6666667@kakao.com>
2020-08-26 21:32:18 +0900

    일정 CRUD 완성~!~!
    일정편집API까지 완성!
    이제
    -일정생성
    -일정조회
    -일정삭제
    -일정편집
    이 가능한 기본 페이지가 완성되었다. 짝짝

--- my_project/app.py
+++ my_project/app.py
@@ -43,49 +43,72 @@ def make_sche():
         'alert2_time': alert2_time,
         'memo': memo
     }
-    print(doc)
     db.todo.insert_one(doc)
     return jsonify({'result': 'success', 'msg': '작성하신 대로 스케줄을 저장했습니다. 멍!'})
+
 
 # 2. 일정 조회(Read) - /readsche (GET)
 @app.route('/readsche', methods=['GET'])
 def read_sche():
     todolist = list(db.todo.find({}))
-    return dumps({'result': 'success', 'todolist': todolist,'msg': '일정을 불러왔습니다. 멍!'})
+    return dumps({'result': 'success', 'todolist': todolist, 'msg': '일정을 불러왔습니다. 멍!'})
+
 
 # 3. 일정 검색(Read?) - /findsche (POST)
 @app.route('/findsche', methods=['POST'])
 def find_sche():
     keyword = request.form['keyword']
     searched = list(db.todo.find({'todo': keyword}, {'_id': 0}))
-    return jsonify({'result': 'success', 'searched': searched,'msg': '검색 완료!'})
+    return jsonify({'result': 'success', 'searched': searched, 'msg': '검색 완료!'})
+
 
 # 4-1. 일정 변경을 위한 조회(find-one) - /readasche (GET)
 @app.route('/readasche', methods=['GET'])
 def read_a_sche():
     fix_id = request.args.get('id')
-    fix_todo = db.todo.find_one({"_id": ObjectId(fix_id)}, {'_id': 0})
-    return jsonify({'result': 'success', 'fix_todo': fix_todo})
+    fix_todo = db.todo.find_one({"_id": ObjectId(fix_id)})
+    return dumps({'result': 'success', 'fix_todo': fix_todo})
+#이스케이핑 된 데이터를 까보는 것.
 
 # 4-2. 일정 변경(Update) - /fixsche (POST)
 @app.route('/fixsche', methods=['POST'])
 def fix_sche():
-    fix_keyword = request.form['fix_keyword']
-    fix_todo = list(db.todo.find_one({'todo': fix_keyword}, {'_id': 0}))
-    return jsonify({'result': 'success', 'fix_todo': fix_todo})
+    id = request.form['id']
+    todo = request.form['todo']
+    start_date = request.form['start_date']
+    start_time = request.form['start_time']
+    end_date = request.form['end_date']
+    end_time = request.form['end_time']
+    memo = request.form['memo']
+    print(memo)
+    # startend = request.form['startend_fix']
+    # alert1_day = request.form['alert1_day_fix']
+    # alert1_time = request.form['alert1_time_fix']
+    # alert2_day = request.form['alert2_day_fix']
+    # alert2_time = request.form['alert2_time_fix']
+    db.todo.update_one({'_id': ObjectId(id)}, {'$set': {
+        'todo': todo,
+        'start_date': start_date,
+        'start_time': start_time,
+        'end_date': end_date,
+        'end_time': end_time,
+        'memo': memo
+    }})
+    return jsonify({'result': 'success', 'msg': '수정된 스케줄을 저장했습니다. 멍!'})
 
 # 5. 일정 삭제(Delete) - /delsche(POST)
 @app.route('/delsche', methods=['POST'])
 def del_sche():
     del_todo = request.form['todo']
-    db.todo.delete_one({'todo':del_todo})
+    db.todo.delete_one({'todo': del_todo})
     return jsonify({'result': 'success', 'msg': '선택하신 일정을 삭제했어요!'})
+
 
 # 6. 알림 보내기?
 # 6-1. 이메일보내기
 # 6-2. 카톡 플친으로 메시지보내기
 ###위의 것들 다 했다면###
-# 7. 날씨 조회 /wether (GET)
+# 7. 날씨 조회 /weather (GET)
 # 8. 회원시스템 개발시작!
 
 
